eb379b28a30029b28b997a95000bce699b18b873
Andrew Blaikie <ablaikie103@gmail.com>
2023-06-26 20:05:36 -0400

    changed path

--- src/tests/test_train.py
+++ src/tests/test_train.py
@@ -6,8 +6,8 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from mlem.api import load
-from src.utils.logs import MyLogger
-from src.scripts.train import train_diabetes_model
+from utils.logs import MyLogger
+from scripts.train import train_diabetes_model
 
 
 @pytest.fixture
